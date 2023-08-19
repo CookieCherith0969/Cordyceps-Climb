e19294b66e7f10807521b8a9314fdd78194ab872
CookieHudson <cookiecherith@gmail.com>
2023-08-19 15:41:43 +1000

    Added new Player to Room Test

--- Cordyceps-Climb/Assets/Scripts/TransitionScript.cs
+++ Cordyceps-Climb/Assets/Scripts/TransitionScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TransitionScript : MonoBehaviour
 {
     RoomScript parent;
-
     Transform marker;
     // Start is called before the first frame update
     void Start()
